0566541440db4833faab4cebdad214d61675a81a
MatiasMonteros09 <matiasmonteros52@gmail.com>
2026-02-20 23:37:55 -0300

    Feat: update tasks model (descripcion, completada)

--- backend/database.js
+++ backend/database.js
@@ -12,13 +12,14 @@ const db = new sqlite3.Database(dbPath, (err) => {
     console.log("Conectado a SQLite");
 
     // Crear tabla si no existe
-    db.run(`
-      CREATE TABLE IF NOT EXISTS tareas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        titulo TEXT NOT NULL,
-        fecha TEXT NOT NULL
-      )
-    `);
+db.run(`
+  CREATE TABLE IF NOT EXISTS tareas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    descripcion TEXT,
+    fecha TEXT,
+    completada INTEGER DEFAULT 0)`
+    );
   }
 });
 
