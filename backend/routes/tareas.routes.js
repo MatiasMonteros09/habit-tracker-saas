d6c7a2332af3ccc51674984907f22f2effe37d2e
MatiasMonteros09 <matiasmonteros52@gmail.com>
2026-02-20 23:01:56 -0300

    Fix: routes names aligned with controller

--- backend/routes/tareas.routes.js
+++ backend/routes/tareas.routes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const tareasController = require("../controllers/tareas.controller");
 
-router.get("/", tareasController.getTareas);
-router.post("/", tareasController.createTarea);
-router.delete("/:id", tareasController.deleteTarea);
-router.put("/:id", tareasController.updateTarea);
+router.get("/", tareasController.obtenerTareas);
+router.get("/:id", tareasController.obtenerTareaPorId);
+router.post("/", tareasController.crearTarea);
 
 module.exports = router;
